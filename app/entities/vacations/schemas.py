dc3f712936d2fbfbca066c68b5dce1d17ea080d2
Artem Zaitsev <artemis.zaytsev@gmail.com>
2025-11-15 15:40:03 +0300

    [feat] Add Vacation start and end dates validation

--- app/entities/vacations/schemas.py
+++ app/entities/vacations/schemas.py
@@ -1,14 +1,25 @@
-from datetime import date
+from datetime import date, timedelta
 
-from pydantic import Field
+from pydantic import Field, model_validator
 
-from app.entities.common.schema import SchemaBase, partial_model
+from app.entities.common.schema import SchemaBase, Validate, partial_model
 
 
 class VacationBase(SchemaBase):
     employee_id: int = Field(..., description="Employee ID")
     start_day: date = Field(..., description="Date when vacation starts")
     end_day: date = Field(..., description="Date when vacation ends")
+
+    @model_validator(mode="after")
+    def validate_date_range(cls):
+
+        return Validate.dates_range(
+            cls,
+            cls.start_day,
+            cls.end_day,
+            min_delta=timedelta(days=1),
+            max_delta=timedelta(days=365),
+        )
 
 
 class SNewVacation(VacationBase): ...
